283b0e8103a5a66cde440a1b8dbf4f405a9bead9
erenofc28 <rcdanteff@gmail.com>
2024-05-11 16:20:24 +0530

    d

--- frontend/react-app/src/stateProvider.tsx
+++ frontend/react-app/src/stateProvider.tsx
@@ -1,13 +1,12 @@
-import {createContext,useContext,useReducer} from "react";
+import {createContext,useContext} from "react";
 // import { initialState } from './reducer';
-
 
 
 export const StateContext = createContext("");
 
 // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
-export const StateProvider = ({reducer, initialState, children }:{reducer:any; initialState:any; children:any;} ) => (
-    <StateContext.Provider value ={useReducer(reducer, initialState)}>
+export const StateProvider = ({initialState, children }:{reducer:any; initialState:any; children:any;} ) => (
+    <StateContext.Provider value={(initialState)}>
       {children}
     </StateContext.Provider>
   );
